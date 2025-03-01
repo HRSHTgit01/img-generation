3d8f15f0741472945c6c1f9add89bdc1307b48e1
Harshit Shukla <harshitshukla@Harshits-MacBook-Pro.local>
2025-03-01 17:54:40 +0530

    commit message

--- server/index.js
+++ server/index.js
@@ -9,11 +9,7 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: 'http://localhost:5173', // Allow your frontend domain
-  methods: 'GET,POST,PUT,DELETE',
-  allowedHeaders: 'Content-Type'
-}));
+app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);
